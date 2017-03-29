90b8c30ff7e70d2b186d4f5a363e428b49e22ad8
mickeyzhou <305816643@qq.com>
2017-03-29 08:18:42 +0000

    modify server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app = express();
 var strftime = require("strftime");
 
 app.get('/', function(req, res){
-    res.send('https://murmuring-plateau-27272.herokuapp.com/December%2015,%202015\
-https://murmuring-plateau-27272.herokuapp.com/1450137600');    
+    res.send('<div>https://murmuring-plateau-27272.herokuapp.com/December%2015,%202015</div>\
+<div>https://murmuring-plateau-27272.herokuapp.com/1450137600</div>');    
 });
 
 app.get('/:time', function(req, res){
     var param = req.params.time;
-    console.log(param);
-    var oDate = new Date(+param);
+    //console.log(param);
+    var oDate = /^\d+$/.test(param) ? (new Date(+param)) : (new Date(param));
     console.log(oDate.toString());
     if(oDate.toString() !== 'Invalid Date'){
         res.send({
