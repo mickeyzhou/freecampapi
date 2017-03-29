6af70c161a4868f32ee2b3526cafb22325e7ba26
mickeyzhou <305816643@qq.com>
2017-03-29 07:46:01 +0000

    deploy apps

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 var express = require("express");
 var app = express();
+var strftime = require("strftime");
 
-app.get('/', function(req, res){
-    res.send('hello world'); 
+app.get('/:time', function(req, res){
+    var param = req.params.time;
+    console.log(param);
+    var oDate = new Date(+param);
+    console.log(oDate.toString());
+    if(oDate.toString() !== 'Invalid Date'){
+        res.send({
+            unix: oDate.getTime(),
+            natural: strftime('%B %d, %Y', oDate)
+        });
+    }else{
+        res.send({
+            unix: null,
+            natural: null
+        });
+    }
 });
 app.listen(8080);
